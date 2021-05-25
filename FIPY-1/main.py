9cbafcfa9988a8f725543af1c70d4b3ff76040da
Yi Feng <19019278@myrp.edu.sg>
2021-05-25 18:01:58 +0800

    organise into fipy 1 & 2

--- FIPY-1/main.py
+++ FIPY-1/main.py
@@ -1,16 +1,15 @@
 from pycoproc_2 import Pycoproc
-from SI7006A20 import SI7006A20
+
 from MPL3115A2 import MPL3115A2
-from customDateTime import customDateTime
+
 import time
 
 
 py = Pycoproc()
-lt = SI7006A20(py)
+
 lp = MPL3115A2(py)
 
-
-
-pybytes.send_signal(2, str(customDateTime()) + ": " + "Sensor Temperature: " + str(lp.temperature()))
-time.sleep(3)
-pybytes.send_signal(2, str(customDateTime()) + ": " + "External Temperature: " + str(lt.temperature()))
+i = 0
+while i = 0:
+    pybytes.send_signal(2, lp.temperature())
+    time.sleep(10)

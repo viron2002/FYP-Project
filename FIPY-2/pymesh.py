0d4a2c484bf36740a9f352f365f014db4d669b7e
viron2002 <19026464@myrp.edu.sg>
2021-07-07 13:58:46 +0800

    pymesh activation

---
+++ FIPY-2/pymesh.py
@@ -0,0 +1,9 @@
+import time
+
+if pybytes is not None:
+    if pybytes.__pymesh and pybytes.__pymesh.__pymesh:
+        pymesh = pybytes.__pymesh.__pymesh
+        while True:
+            pkt = "Hello, from " + str(pymesh.mac())
+            pybytes.send_signal(1, pkt)
+            time.sleep(20)

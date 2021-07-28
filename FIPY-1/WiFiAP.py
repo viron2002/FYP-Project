4edb768dd6301419152fbe6fba5d01c9c0b05c18
19019278ThumYiFeng <19019278@myrp.edu.sg>
2021-07-28 13:30:23 +0800

    AP

--- FIPY-1/WiFiAP.py
+++ FIPY-1/WiFiAP.py
@@ -1,7 +1,18 @@
-from network import WLAN
-wlan = WLAN()
 
+def wifiAP(ap): 
+    from network import WLAN
+    wlan = WLAN()
 
-wlan.init(mode=WLAN.AP, ssid='FIPY1')
+    if ap == True:
+        #Password 8 letters
+        wlan.init(mode=WLAN.AP, ssid="FIPY1", auth=(WLAN.WPA2, "1a2b3c4d"))
+        wlan.ifconfig(id=1)
+        print("AP Mode\n") #id=1 signifies the AP interface
 
-print(wlan.ifconfig(id=1)) #id =1 signifies the AP interface
+    elif ap == False:
+        wlan.ifconfig(id=0)
+        print("Station Mode\n")
+
+    else:
+        print("Invalid")
+

6a06b7b489562d2ba8110945956d5f3079a881ba
viron2002 <82698240+viron2002@users.noreply.github.com>
2021-08-07 10:42:59 +0800

    Add files via upload

--- FIPY-2/main.py
+++ FIPY-2/main.py
@@ -1,7 +1,24 @@
-from class1 import abc
+from SI7006A20 import SI7006A20
+import math
+
+def connection_status():
+        if pybytes.isconnected():
+            print("Connected")
+        else :
+            pybytes.connect()
+            print("Need to have activation codet to connect.")
 
 connection_status()
-print("")
-display()
-print("")
-wifiAP()
+
+def display_data():
+        # from SI7006A20 import SI7006A20
+        obj = SI7006A20()
+        print("Humidity in % is in pybytes. ")
+        value1 = obj.humidity()
+        pybytes.send_signal(3, math.ceil(value1))
+        print("")
+        print("Temperature in degree is pybytes. ")
+        value2 = obj.temperature()
+        pybytes.send_signal(4, math.ceil(value2))
+
+display_data()
